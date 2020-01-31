9ec2a6fd218981841cdb385cf21d2ef9595d7cf4
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-01-31 21:43:22 +0500

    Выполнил больше деталей (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,40 +1,120 @@
 'use strict';
 
-var typeOfRoom = ['palace', 'flat', 'house', 'bungalo'];
-var checkinTime = ['12:00', '13:00', '14:00'];
-var checkoutTime = ['12:00', '13:00', '14:00'];
-
-var avatarNumber = [
-    'img/avatars/user01.png', 
-    'img/avatars/user02.png', 
-    'img/avatars/user03.png', 
-    'img/avatars/user04.png', 
-    'img/avatars/user05.png', 
-    'img/avatars/user06.png', 
-    'img/avatars/user07.png', 
-    'img/avatars/user08.png'
+var titles = [
+  'Сдаю хату',
+  'Комната по бросовой цене',
+  'Суперквартира', 'Райское жилище',
+  'Шалаш со всеми удобствами'
 ];
+var typeOfRoom = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+var checkTime = ['12:00', '13:00', '14:00'];
+var specification = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+var characteristic = [
+  'Без тараканов и клопов',
+  'Деревянный туалет на улице',
+  'Имеется прислуга',
+  'Есть бочка-офуро'
+];
+var photosOfRoom = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+var COUNTER = 8;
 
-var getAvatar = function () {
-    var authors = [];
-
-    for (var i = 0; i < avatarNumber.length; i++) {
-        authors[i] = {
-            avatar: avatarNumber[i]
-          };
-    }
-  return authors;
+var randomInteger = function (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
 };
 
-var getOffer = function () {
-    var offer = {
-        title: '',
-        address: '600, 350',
-        price: 100,
-        type: ,
-        rooms: ,
-        guests: ,
-        checkin: ,
-        checkout: ,
+var getFeaturesArray = function () {
+  var newFeaturesArray = [];
+
+  for (var i = 0; i < randomInteger(1, specification.length); i++) {
+    newFeaturesArray[i] = specification[i];
+  }
+  return newFeaturesArray;
+};
+
+var featuresArray = getFeaturesArray();
+
+var getPhotosArray = function () {
+  var newPhotosArray = [];
+
+  for (var i = 0; i < randomInteger(1, photosOfRoom.length); i++) {
+    newPhotosArray[i] = photosOfRoom[i];
+  }
+  return newPhotosArray;
+};
+
+var photosArray = getPhotosArray();
+
+var generateData = function (count) {
+  var arr = [];
+
+  for (var i = 0; i < count; i++) {
+    arr[i] = {
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      offer: {
+        title: titles[randomInteger(0, titles.length - 1)],
+        address: 'location.x, location.y',
+        price: randomInteger(0, 1000),
+        type: typeOfRoom[randomInteger(0, typeOfRoom.length - 1)],
+        rooms: randomInteger(1, 5),
+        guests: randomInteger(1, 10),
+        checkin: checkTime[randomInteger(0, checkTime.length - 1)],
+        checkout: checkTime[randomInteger(0, checkTime.length - 1)],
+        features: featuresArray,
+        description: characteristic[randomInteger(0, characteristic.length - 1)],
+        photos: photosArray
+      },
+      location: {
+        x: randomInteger(130, 800),
+        y: randomInteger(130, 630)
+      }
     };
-}
+  }
+  return arr;
+};
+
+document.querySelector('.map').classList.remove('map--faded');
+
+var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var similarListPin = document.querySelector('.map__pins');
+
+var renderPin = function (data) {
+  var pinElement = similarPinTemplate.cloneNode(true);
+
+  pinElement.querySelector('.map__pin').style.left = data.location.x;
+  pinElement.querySelector('.map__pin').style.top = data.location.y;
+  pinElement.querySelector('.map__pin').content.querySelector('img').src = data.author.avatar;
+  pinElement.querySelector('.map__pin').content.querySelector('img').alt = data.offer.title;
+
+  similarListPin.appendChild(pinElement);
+};
+
+var showSetup = function () {
+  var datas = generateData(COUNTER);
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < datas.length; i++) {
+    fragment.appendChild(renderPin(datas[i]));
+  }
+  similarListPin.appendChild(fragment);
+};
+
+showSetup();
