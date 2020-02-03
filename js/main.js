e77ce861e10c1bf177b4be8926fd69f8267cad4e
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-02-03 17:19:06 +0500

    Доделал задание больше деталей часть 2

--- js/main.js
+++ js/main.js
@@ -144,30 +144,53 @@ showSetupPins();
 var similarCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var similarListCard = document.querySelector('.map');
 
+var renderFeatures = function (container, features) {
+  container.innerHTML = '';
+
+  for (var i = 0; i < features.length; i++) {
+    var createLi = document.createElement('li');
+    createLi.classList.add('popup__feature');
+    createLi.classList.add('popup__feature--' + features[i]);
+    container.appendChild(createLi);
+  }
+};
+
+var renderPhotos = function (container, node, photos) {
+  container.innerHTML = '';
+
+  for (var i = 0; i < photos.length; i++) {
+    var img = node.cloneNode();
+    img.src = photos[i];
+    container.appendChild(img);
+  }
+};
+
 var renderCard = function (data) {
   var cardElement = similarCardTemplate.cloneNode(true);
 
   cardElement.querySelector('.popup__title').textContent = data.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = data.offer.addres;
-  cardElement.querySelector('.popup__text--price').textContent = data.offer.price + 'Р/ночь';
+  cardElement.querySelector('.popup__text--price').textContent = data.offer.price + ' Р/ночь';
   cardElement.querySelector('.popup__type').textContent = data.offer.type;
   cardElement.querySelector('.popup__text--capacity').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
   cardElement.querySelector('.popup__description').textContent = data.offer.description;
-  cardElement.querySelector('.popup__photos').querySelector('img').src = data.offer.photos;
   cardElement.querySelector('.popup__avatar').src = data.author.avatar;
+
+  renderFeatures(cardElement.querySelector('.popup__features'), data.offer.features);
+  renderPhotos(cardElement.querySelector('.popup__photos'), cardElement.querySelector('.popup__photo'), data.offer.photos);
 
   return cardElement;
 };
 
 var showSetupCards = function () {
   var datas = generateData(COUNTER);
-
+  var mapFiltersContainer = document.querySelector('.map__filters-container');
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < datas.length; i++) {
     fragment.appendChild(renderCard(datas[i]));
   }
-  similarListCard.appendChild(fragment);
+  similarListCard.insertBefore(fragment, mapFiltersContainer);
 };
 
 showSetupCards();
