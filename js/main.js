0228c11a48766ab691dce1e5d4e46617a816f0ba
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-02-08 21:24:53 +0500

    Сделал всё, кроме валидации формы

--- js/main.js
+++ js/main.js
@@ -137,8 +137,6 @@ var showSetupPins = function () {
   similarListPin.appendChild(fragment);
 };
 
-showSetupPins();
-
 // Отрисовка карточки
 // var similarCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 // var similarListCard = document.querySelector('.map');
@@ -196,25 +194,38 @@ showSetupPins();
 
 // Активация страницы
 var ENTER_KEY = 'Enter';
+var labelCenterTop = document.querySelector('.map__pin--main').style.top;
+var labelCenterLeft = document.querySelector('.map__pin--main').style.left;
+var addressBar = document.getElementById('address');
 
-document.querySelector('.ad-form-header').classList.add('disabled');
-document.querySelector('.map__filters').classList.add('disabled');
+addressBar.placeholder = Math.round(parseInt(labelCenterLeft, 10) + 32.5) + ', ' + Math.round(parseInt(labelCenterTop, 10) + 32.5);
+
+var getAddressBar = function () {
+  var spikeLabelTop = Math.round(parseInt(labelCenterTop, 10) + 78);
+  var spikeLabelLeft = Math.round(parseInt(labelCenterLeft, 10) + 32.5);
+
+  addressBar.placeholder = spikeLabelLeft + ', ' + spikeLabelTop;
+};
+
+document.querySelector('.ad-form-header').setAttribute('disabled', 'disabled');
+document.querySelector('.map__filters').classList.add('ad-form--disabled');
 
 var selectionFieldset = document.getElementsByClassName('ad-form__element');
 
 for (var i = 0; i < selectionFieldset.length; i++) {
-  selectionFieldset[i].classList.add('disabled');
+  selectionFieldset[i].setAttribute('disabled', 'disabled');
 }
 
 var mainMark = document.querySelector('.map__pin--main');
 
 var removeDisabled = function () {
+  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
   document.querySelector('.map').classList.remove('map--faded');
-  document.querySelector('.ad-form-header').classList.remove('disabled');
-  document.querySelector('.map__filters').classList.remove('disabled');
+  document.querySelector('.ad-form-header').removeAttribute('disabled');
+  document.querySelector('.map__filters').classList.remove('ad-form--disabled');
 
   for (var j = 0; j < selectionFieldset.length; j++) {
-    selectionFieldset[j].classList.remove('disabled');
+    selectionFieldset[j].removeAttribute('disabled');
   }
 };
 
@@ -230,11 +241,15 @@ var detectLeftButton = function (evt) {
 mainMark.addEventListener('mousedown', function () {
   if (detectLeftButton()) {
     removeDisabled();
+    showSetupPins();
+    getAddressBar();
   }
 });
 
 mainMark.addEventListener('keydown', function (evt) {
   if (evt.key === ENTER_KEY) {
     removeDisabled();
+    showSetupPins();
+    getAddressBar();
   }
 });
