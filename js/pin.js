865594ca517a5e35ca4f2b05009971a3ac424551
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-02-19 16:12:58 +0500

    Личный проект: модуляция часть 2

--- js/pin.js
+++ js/pin.js
@@ -4,16 +4,14 @@
 (function () {
   var labelCenterTop = document.querySelector('.map__pin--main').style.top;
   var labelCenterLeft = document.querySelector('.map__pin--main').style.left;
-  var addressBar = document.querySelector('#address');
-  var mainMark = document.querySelector('.map__pin--main');
 
-  addressBar.placeholder = Math.round(parseInt(labelCenterLeft, 10) + 32.5) + ', ' + Math.round(parseInt(labelCenterTop, 10) + 32.5);
+  window.utils.addressBar.placeholder = Math.round(parseInt(labelCenterLeft, 10) + 32.5) + ', ' + Math.round(parseInt(labelCenterTop, 10) + 32.5);
 
   var getAddressBar = function () {
     var spikeLabelTop = Math.round(parseInt(labelCenterTop, 10) + 78);
     var spikeLabelLeft = Math.round(parseInt(labelCenterLeft, 10) + 32.5);
 
-    addressBar.placeholder = spikeLabelLeft + ', ' + spikeLabelTop;
+    window.utils.addressBar.placeholder = spikeLabelLeft + ', ' + spikeLabelTop;
   };
 
   var detectLeftButton = function (evt) {
@@ -25,19 +23,41 @@
     return button === 1;
   };
 
-  mainMark.addEventListener('mousedown', function () {
-    if (detectLeftButton()) {
+  window.utils.mainMark.addEventListener('keydown', function (evt) {
+    if (evt.key === window.utils.ENTER_KEY) {
       window.form.removeDisabled();
       window.map.showSetupPins();
       getAddressBar();
     }
   });
 
-  mainMark.addEventListener('keydown', function (evt) {
-    if (evt.key === window.utils.ENTER_KEY) {
+  window.utils.mainMark.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    if (detectLeftButton()) {
       window.form.removeDisabled();
       window.map.showSetupPins();
       getAddressBar();
     }
+
+    window.startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      window.calc.getCalcCoords(moveEvt);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      window.calc.getCalcCoords(upEvt);
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
--- js/utils.js
+++ js/utils.js
@@ -3,9 +3,13 @@
 (function () {
   var ENTER_KEY = 'Enter';
   var ESC_KEY = 'Escape';
+  var addressBar = document.querySelector('#address');
+  var mainMark = document.querySelector('.map__pin--main');
 
   window.utils = {
     ENTER_KEY: ENTER_KEY,
-    ESC_KEY: ESC_KEY
+    ESC_KEY: ESC_KEY,
+    addressBar: addressBar,
+    mainMark: mainMark
   };
 })();
